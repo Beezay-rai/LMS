9c0a10014a39a29948bea437b3b892c2b0d0e38c
Yuki <beezayrae@gmail.com>
2023-10-29 17:07:19 +0545

    borrow

--- Data/Borrow.cs
+++ Data/Borrow.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LMS.Data
 {
@@ -6,5 +7,23 @@ namespace LMS.Data
     {
         [Key]
         public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [ForeignKey(nameof(UserId))]  
+        public User User { get; set; }
+        [Required]
+        public int BookId { get; set;}
+        [ForeignKey(nameof(BookId))]
+        public Book Book { get; set; }
+        [Required,DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+
+        public bool? Deleted { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string? DeletedBy { get; set; }
+        public DateTime? DeletedDate { get; set; }
     }
 }
